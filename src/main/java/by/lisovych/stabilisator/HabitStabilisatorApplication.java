98a182c1578df3a7806e0262b9a923eb33e4294b
alexander lisovich <lisovyhs@gmail.com>
2025-11-21 05:44:25 +0200

    first commit

---
+++ src/main/java/by/lisovych/stabilisator/HabitStabilisatorApplication.java
@@ -0,0 +1,13 @@
+package by.lisovych.stabilisator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitStabilisatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitStabilisatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/lisovych/stabilisator/StabilisatorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lisovych.stabilisator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StabilisatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
